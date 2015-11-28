e862aa87c5b1af3e572623fc80b104a483b85c34
Toby Smyth <tobs169@gmail.com>
2015-11-28 11:40:26 +0000

    Serve pure

--- src/ElmApp.hs
+++ src/ElmApp.hs
@@ -26,7 +26,7 @@ elmImport :: ElmApp -> Html
 elmImport  (ElmApp l) = script "" ! src  (stringValue $ l ++ "/elm.js" ) ! type_ "text/javascript"
 
 elmCss :: Html
-elmCss = link ! rel "stylesheet" ! href "http://yui.yahooapis.com/pure/0.6.0/pure-min.css"
+elmCss = link ! rel "stylesheet" ! href "static/css/pure.css"
 
 elmMount :: Html
 elmMount = script "Elm.fullscreen(Elm.Main)" ! type_ "text/javascript"

ee46eaabffe0569fc8c6cd08bc08aad1a10923cd
Toby Smyth <tobs169@gmail.com>
2016-01-07 17:21:17 +0000

    Don't init

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,12 @@
 var gulp = require('gulp');
 var elm = require('gulp-elm');
 
-gulp.task('elm-init', elm.init)
-
-gulp.task('elm', ['elm-init'], function () {
+gulp.task('elm', function () {
   return gulp.src('elm/Main.elm')
     .pipe(elm())
     .pipe(gulp.dest('dist/'));
 });
 
-gulp.task('default')
+gulp.task('default', ['elm'])
 
 gulp.task('heroku:prod', ['default']);

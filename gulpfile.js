ceb0f2ed44ecc1ad0d027e43a568ccf075827da0
Toby Smyth <tobs169@gmail.com>
2016-01-13 07:47:27 +0000

    webpack deps

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 
 var gulp = require('gulp');
 var purescript = require('gulp-purescript');
-
+var webpack = require('webpack-stream');
 var sources = [
   "static/purescript/src/**/*.purs",
   "bower_components/purescript-*/src/**/*.purs"
@@ -12,13 +12,24 @@ var foreigns = [
   "bower_components/purescript-*/src/**/*.js"
 ];
 
+var pscBundle = "static/dist/psc-bundle.js";
+
 gulp.task('make', function () {
   return purescript.psc({src: sources, ffi: foreigns});
 });
 
-gulp.task('bundle', ['make'], function (){
-  return purescript.pscBundle({src: "output/**/*.js", output: "static/dist/bundle.js"});
-})
+gulp.task('psc-bundle', ['make'], function (){
+  return purescript.pscBundle({src: "output/**/*.js", output: pscBundle});
+});
+
+gulp.task('bundle', ['psc-bundle'], function (){
+  return gulp.src(pscBundle)
+    .pipe(webpack({
+      resolve: {moduleDirectories: ["node_modules"]},
+      output: {filename: "bundle.js"}
+    }))
+    .pipe(gulp.dest("static/dist"));
+});
 
 gulp.task('default', ['bundle'])
 

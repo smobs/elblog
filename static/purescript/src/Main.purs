31a172fc7959e4436c63e7c6c3ac7cd9fe6b7121
Toby Smyth <tobs169@gmail.com>
2016-01-13 19:29:23 +0000

    Demo halogen app

--- static/purescript/src/Main.purs
+++ static/purescript/src/Main.purs
@@ -1,6 +1,49 @@
 module Main where
 
-import Control.Monad.Eff.Console
-import Halogen
+import Prelude
+import Control.Monad.Aff (runAff)
+import Control.Monad.Eff (Eff())
+import Control.Monad.Eff.Exception (throwException)
+import Control.Plus (Plus)
 
-main = log "Hello blog"
+import Halogen
+import Halogen.Util (appendToBody, onLoad)
+import qualified Halogen.HTML.Indexed as H
+import qualified Halogen.HTML.Events.Indexed as E
+
+
+main :: Eff (HalogenEffects ()) Unit
+main = runAff throwException (const (pure unit)) do
+  app <- runUI myComponent {on: false}
+  onLoad $ appendToBody app.node
+
+-- | The state of the component
+type State = { on :: Boolean }
+
+-- | The query algebra for the component
+data Query a
+  = ToggleState a
+  | GetState (Boolean -> a)
+
+-- | The component definition
+myComponent :: forall g. (Functor g) => Component State Query g
+myComponent = component render eval
+  where
+
+  render :: State -> ComponentHTML Query
+  render state =
+    H.div_
+      [ H.h1_
+          [ H.text "Toggle Button" ]
+      , H.button
+          [ E.onClick (E.input_ ToggleState) ]
+          [ H.text (if state.on then "On" else "Off") ]
+      ]
+
+  eval :: Natural Query (ComponentDSL State Query g)
+  eval (ToggleState next) = do
+    modify (\state -> { on: not state.on })
+    pure next
+  eval (GetState continue) = do
+    value <- gets _.on
+    pure (continue value)

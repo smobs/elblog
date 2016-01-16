aae52c2c46d509de1aeaa711edbee324a6683b98
Toby Smyth <tobs169@gmail.com>
2016-01-16 20:21:52 +0000

    Serve blogs

--- server/app/Main.hs
+++ server/app/Main.hs
@@ -1,8 +1,12 @@
 {-# LANGUAGE DataKinds     #-}
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE TypeOperators #-}
+
 module Main where
 
+import           Data.Aeson
 import           Data.Maybe               (fromMaybe)
+import           GHC.Generics
 import           Network.Wai
 import           Network.Wai.Handler.Warp
 import           PSApp
@@ -18,10 +22,12 @@ main = do
 app :: Application
 app = serve siteAPI server
 
+data Blog = Blog {title :: String, content :: String} deriving (Generic, Eq, Show)
 
-type Blog = String
+instance ToJSON Blog
 
-type BlogApi = "api" :> "blog" :> Get '[JSON] Blog
+type BlogApi = "api" :> "blogs" :> Get '[JSON] [Int]
+     :<|> "api" :> "blog" :> Capture "blogId"  Int :> Get '[JSON] Blog
 
 type HomeApi = Get '[HTML] PSApp
 
@@ -29,9 +35,12 @@ type SiteApi = HomeApi :<|> BlogApi :<|> "static" :> Raw
 
 server ::  Server SiteApi
 server = return (PSApp "static/dist")
-         :<|> return "Hello world"
-         :<|>  serveDirectory "static"
+         :<|> (return [1,2,3]
+         :<|> blogHandler)
+         :<|> serveDirectory "static"
 
+blogHandler id = let i = show id
+                 in return $ Blog ("Blog " ++ i) ("Content " ++ i)
 
 siteAPI :: Proxy SiteApi
 siteAPI = Proxy

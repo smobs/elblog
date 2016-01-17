d2357590d684190035887d7f906293d5f64b9d97
Toby Smyth <tobs169@gmail.com>
2016-01-17 00:36:08 +0000

    Ajax on load

--- static/purescript/src/Component/Article.purs
+++ static/purescript/src/Component/Article.purs
@@ -4,14 +4,19 @@ import Prelude
 import Halogen
 import qualified Halogen.HTML.Indexed as H
 import qualified Halogen.HTML.Events.Indexed as E
+import qualified Halogen.HTML.Properties.Indexed as P
 import Control.Monad.Aff (Aff())
 
 import Network.HTTP.Affjax (AJAX())
+import qualified Network.HTTP.Affjax as Ajax
+
 import Model
 
 type State = Article
 
 data Query a = Toggle a
+             | Load Int a
+
 type ArticleEffects eff = HalogenEffects (ajax :: AJAX | eff)
 
 article :: forall eff. Component State Query (Aff (ArticleEffects eff))
@@ -20,7 +25,9 @@ article = component render eval
 
   render :: State -> ComponentHTML Query
   render state =
-    let title = H.h2 [E.onClick (E.input_ Toggle)] [H.text state.title] in
+    let title = H.h2 [E.onClick (E.input_ Toggle)
+                     , P.initializer \_ -> action (Load state.id)]
+                [H.text state.title] in
     H.div_
       if state.visible
       then
@@ -34,7 +41,17 @@ article = component render eval
   eval (Toggle next) = do
     modify (\s -> s {visible = not s.visible})
     pure next
+  eval (Load id next) = do
+    c <- liftAff' $ getBlog id
+    modify (\s -> s {contents = c})
+    pure next
 
+getBlog :: forall eff. Int -> Aff (ajax :: AJAX | eff) String
+getBlog id =
+  let url = "api/blog/" ++ show id in
+  do
+    res <- Ajax.get url
+    return res.response
 
 
 
--- static/purescript/src/Model.purs
+++ static/purescript/src/Model.purs
@@ -6,11 +6,11 @@ type ArticleId = Int
 
 type Blog = {articles :: Array ArticleId}
 
-type Article = {contents :: String, visible :: Boolean, title :: String}
+type Article = {contents :: String, visible :: Boolean, title :: String, id :: ArticleId}
 
 initialBlog :: Blog
 initialBlog = {articles : [1, 2, 13]}
 
 initialArticle :: ArticleId -> Article
 initialArticle i = let id = show i in
-  {title : "Title" ++ id, visible : false, contents : "Hello " ++ id}
+  {title : "Title" ++ id, visible : false, contents : "Hello " ++ id, id : i}

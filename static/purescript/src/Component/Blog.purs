e0b1092802a5708eab1a2c89b36c2b174b88a7bc
Toby Smyth <tobs169@gmail.com>
2016-01-17 20:15:21 +0000

    Get blog list from server

--- static/purescript/src/Component/Blog.purs
+++ static/purescript/src/Component/Blog.purs
@@ -8,11 +8,19 @@ import Data.Functor.Coproduct (Coproduct())
 import Halogen
 import qualified Halogen.HTML.Indexed as H
 import qualified Halogen.HTML.Events.Indexed as E
+import qualified Halogen.HTML.Properties.Indexed as P
 
 import Model
 import Component.Article (article)
 import qualified Component.Article as Article
 import Control.Monad.Aff (Aff())
+import Network.HTTP.Affjax (AJAX())
+import qualified Network.HTTP.Affjax as Ajax
+
+import Data.Foreign
+import Data.Foreign.Class
+import Data.Either (Either(..))
+
 
 type State = Blog
 -- | The state of the component
@@ -28,8 +36,8 @@ type FQuery = Coproduct Query (ChildF ArticleSlot Article.Query)
 
 -- | The query algebra for the component
 data Query a =
-  GetState (State -> a)
-
+  Load a
+    
 -- | The component definition9
 blog :: forall eff. Component (FState (Aff(Article.ArticleEffects eff))) FQuery (Aff(Article.ArticleEffects eff))
 blog = parentComponent render eval
@@ -37,7 +45,7 @@ blog = parentComponent render eval
 
   render :: State -> ParentHTML Article Query Article.Query (Aff(Article.ArticleEffects eff)) ArticleSlot
   render state =
-    H.div_
+    H.div [P.initializer \_ -> action Load]
       [ H.h1_
           [ H.text "Awesome Blog" ]
       , H.div_
@@ -45,10 +53,22 @@ blog = parentComponent render eval
       ]
 
   eval :: Natural Query (ParentDSL State Article.State Query Article.Query (Aff(Article.ArticleEffects eff)) ArticleSlot)
-  eval (GetState continue) = do
-    value <- get
-    pure (continue value)
+  eval (Load a) = do
+    ids <- liftH $ liftAff' getBlogIds
+    modify (\s -> s {articles = ids})
+    pure a
+    
 
   renderArticle :: ArticleId -> ParentHTML Article Query Article.Query (Aff(Article.ArticleEffects eff)) ArticleSlot
-  renderArticle articleId = H.slot (ArticleSlot articleId) \_ -> {component: article, initialState: initialArticle articleId}  
+  renderArticle articleId = H.slot (ArticleSlot articleId) \_ -> {component: article, initialState: initialArticle articleId}
 
+getBlogIds :: forall eff. Aff (ajax :: AJAX | eff) (Array ArticleId)
+getBlogIds = 
+let url = "api/blogs" in
+  do
+    resp <- Ajax.get url
+    return case
+      readJSON resp.response :: F (Array Int)
+      of
+        Right ids -> ids
+        Left _ -> []

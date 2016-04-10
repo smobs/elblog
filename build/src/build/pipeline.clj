b63e7128a252b625938bf81ae3f2f7ee0e70c7e6
Toby Smyth <tobs169@gmail.com>
2016-04-10 21:01:50 +0100

    Init buildserver

---
+++ build/src/build/pipeline.clj
@@ -0,0 +1,15 @@
+(ns build.pipeline
+  (:use [lambdacd.steps.control-flow]
+        [build.steps])
+  (:require
+        [lambdacd.steps.manualtrigger :as manualtrigger]))
+
+(def pipeline-def
+  `(
+    manualtrigger/wait-for-manual-trigger
+    some-step-that-does-nothing
+    (in-parallel
+      some-step-that-echos-foo
+      some-step-that-echos-bar)
+    manualtrigger/wait-for-manual-trigger
+    some-failing-step))

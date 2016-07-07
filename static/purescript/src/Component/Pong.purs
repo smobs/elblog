d68dd3a45814d4807f243ed8de425b03d81b70d3
Toby Smyth <tobs169@gmail.com>
2016-07-07 23:21:17 +0100

    Move the square

--- static/purescript/src/Component/Pong.purs
+++ static/purescript/src/Component/Pong.purs
@@ -16,9 +16,9 @@ import Graphics.Drawing
 import Control.Monad.Eff
 import Control.Monad.Eff.Class
 
-type State = Unit
+type State = {x :: Number, y :: Number}
 
-data Query a = NewGame a
+data Query a = NewGame a | Move a
 
 type PongEffects eff = (canvas :: CANVAS  | eff)
 
@@ -26,16 +26,25 @@ game :: forall g eff. (Functor g, MonadEff (HalogenEffects(PongEffects eff)) g) 
 game = lifecycleComponent {render, eval, initializer: Just (action NewGame), finalizer: Nothing}
             where
               render :: State -> ComponentHTML Query
-              render _ = H.canvas [P.id_ "Foo"]
-              eval :: Query ~> (ComponentDSL State Query g)
-              eval (NewGame a) = do
-                liftH <<< liftEff $  draw
-                pure a
+              render _ = H.canvas [P.id_ "Foo", E.onKeyDown (E.input_  Move), P.tabIndex 0 ]
 
-draw :: forall eff. Eff (canvas :: CANVAS | eff) Int
-draw = do
+              eval :: Natural Query (ComponentDSL State Query g)
+              eval (NewGame a) = do
+                s <- get 
+                liftH <<< liftEff $ draw s
+                return a
+              eval (Move a) = do
+                modify (\s -> s {x= s.x + 1.0, y= s.y + 1.0})
+                s <- get
+                liftH <<< liftEff $ draw s
+                return a
+                
+
+draw :: State -> forall eff. Eff (canvas :: Canvas | eff) Int
+draw s = do
   Just canvas <- getCanvasElementById "Foo"
   ctx <- getContext2D canvas
+  clearRect ctx {x: 0.0, y: 0.0, w: 1000.0, h:1000.0} 
   render ctx $
-    filled (fillColor black) (rectangle 1.0 1.0 60.0 40.0)
-  pure 1
+    filled (fillColor black) (rectangle s.x s.y 60.0 40.0)
+  return 1

e048148ee17d09f0b6fd4a38f8d098fc42872697
Toby Smyth <tobs169@gmail.com>
2016-07-20 19:28:29 +0100

    webpack in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,7 @@
 
 var bower = require('gulp-bower');
 var gulp = require('gulp');
-var purescript = require('gulp-purescript');
 var webpack = require('webpack-stream');
-
-var sources = [
-  "static/purescript/src/**/*.purs",
-  "bower_components/purescript-*/src/**/*.purs"
-];
-
-var foreigns = [
-  "static/purescript/src/**/*.js",
-  "bower_components/purescript-*/src/**/*.js"
-];
-
-var pscBundle = "static/dist/psc-bundle.js";
 
 var cssSources = "static/css/**";
 
@@ -24,31 +11,10 @@ gulp.task('bower', function() {
   return bower();
 });
 
-gulp.task('make', ['bower'], function () {
-  return purescript.psc({src: sources, ffi: foreigns});
-});
-
-gulp.task('psc-bundle', ['make'], function (){
-  return purescript.pscBundle({
-    src: "output/**/*.js",
-    output: pscBundle,
-    module: "Main",
-    main: "Main"
-  });
-});
-
-gulp.task("dotpsci", function () {
-  return purescript.psci({ src: sources, ffi: foreigns })
-    .pipe(gulp.dest("."));
-});
-
-gulp.task('bundle', ['psc-bundle'], function (){
-  return gulp.src(pscBundle)
-    .pipe(webpack({
-      resolve: {moduleDirectories: ["node_modules"]},
-      output: {filename: "bundle.js"}
-    }))
-    .pipe(gulp.dest("static/dist"));
+gulp.task('webpack', ['bower'], function () {
+  return gulp.src('static/entry.js')
+    .pipe(webpack(require('./webpack.config.js')))
+    .pipe(gulp.dest('static/dist/'))
 });
 
 gulp.task('css', function(){
@@ -61,6 +27,6 @@ gulp.task('resources', function () {
     .pipe(gulp.dest("static/dist/resources"));
 });
 
-gulp.task('default', ['resources','css','bundle', 'dotpsci'])
+gulp.task('default', ['resources','css','webpack'])
 
 gulp.task('heroku:prod', ['default']);

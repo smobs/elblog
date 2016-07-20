d5b68ec8d5a404a5cdb1452abec2da586dd7c4a5
Toby Smyth <tobs169@gmail.com>
2016-07-20 19:18:12 +0100

    Build with webpack

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const validate = require("webpack-validator");
+const path = require('path');
+
+const config = 
+      { debug: true
+        , devtool: 'source-map'
+        , entry: './static/entry'
+        , output: {path: path.join(__dirname, "static/dist")
+                   , pathinfo: true
+                   , filename: "bundle.js"
+                  }
+        , module: { loaders: [ { test: /\.purs$/
+                           , loader: 'purs-loader'
+                           , query: { src: [ 'bower_components/purescript-*/src/**/*.purs', 'static/purescript/src/**/*.purs' ]
+                                    , bundle: true
+                                    , psc: 'node_modules/purescript/bin/psc.js'
+                                    , pscBundle: 'node_modules/purescript/bin/psc-bundle.js'
+                                    , pscArgs: { sourceMaps: true}
+                                    , pscIde: true
+                                    }
+                           }
+                         , { test: /\.js$/
+                           , loader: 'source-map-loader'
+                           , exclude: /node_modules|bower_components/
+                           }
+                         ]
+              }
+        , resolve: { modulesDirectories: ['node_modules', 'bower_components'],
+                     extensions: ['', '.js','.purs']
+        }
+};
+
+module.exports = validate(config);
+    

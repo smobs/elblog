f858f7359e642b2515c5c138d24b0e14816d0158
Toby Smyth <tobs169@gmail.com>
2017-01-03 15:06:47 +0000

    easeljs hello world

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const validate = require("webpack-validator");
 const path = require('path');
-
+const webpack = require('webpack');
 const config = 
       { debug: true
         , devtool: 'source-map'
@@ -8,7 +8,7 @@ const config =
                        , port: 4008
                        , stats: 'errors-only'
                      }
-        , entry: './static/entry'
+        , entry: './static/pong'
         , output: {path: path.join(__dirname, "static/dist")
                    , pathinfo: true
                    , filename: "bundle.js"
@@ -22,13 +22,22 @@ const config =
                                     , pscArgs: { sourceMaps: true }
                                     , pscIde: true
                                     }
-                           }
+                               },
+                               // That will tell Webpack that EaselJS refers to `window` with `this` and exports `window.createjs`.
+                               { test: /bower_components\/EaselJS\/.*\.js$/, loader: 'imports?this=>window!exports?window.createjs' }
+                               
                          , { test: /\.js$/
                            , loader: 'source-map-loader'
                            , exclude: /node_modules|bower_components/
                            }
                          ]
-              }
+                  }
+        , plugins: [
+          // This will tell Webpack how to find the main file of bower modules
+          new webpack.ResolverPlugin(
+            new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin('.bower.json', ['main'])
+          )
+        ]
         , resolve: { modulesDirectories: ['node_modules', 'bower_components'],
                      extensions: ['', '.js','.purs']
         }

598e37556c80d5d0bdc9b24cc1a3cc66a1cf9c9a
Toby Smyth <tobs169@gmail.com>
2017-03-11 22:58:47 +0000

    Get on IORef

--- server/app/Main.hs
+++ server/app/Main.hs
@@ -8,11 +8,12 @@ import           Control.Monad.Trans.Reader
 import           Control.Monad.Logger               (runStderrLoggingT)
 import           Data.Aeson
 import           Data.Maybe               (fromMaybe)
+import           Data.IORef
 import           Network.Wai
 import           Network.Wai.Handler.Warp
 import           PSApp
 import           WebApi
-import           Control.Concurrent.STM
+import           Control.Concurrent.STM 
 import           Servant
 import           Servant.HTML.Blaze
 import           System.BlogRepository
@@ -20,9 +21,11 @@ import           System.Environment
 import           Servant.Subscriber.Subscribable
 import           Servant.Subscriber
 
-type ServerData = String
+data ServerData = ServerData {
+  messageRef :: IORef String
+}
 
-type GameHandler = ReaderT ServerData Handler 
+type GameHandler = (ReaderT ServerData Handler) 
 
 transformGameHandler :: ServerData -> (GameHandler :~> Handler)
 transformGameHandler sd = runReaderTNat sd
@@ -31,7 +34,8 @@ main :: IO ()
 main = do
   p <- port
   cd <- atomically (makeSubscriber "subscriber" runStderrLoggingT)
-  run p $ app "Wonderbar" cd
+  ref <- newIORef "Wonderbar"
+  run p $ app (ServerData ref) cd
 
 app :: ServerData -> Subscriber SiteApi -> Application
 app sd sub = serveSubscriber sub (server sd)
@@ -41,9 +45,14 @@ instance ToJSON Blog
 postGameHandler :: String -> ReaderT ServerData Handler ()
 postGameHandler s = pure ()
 
+getGameHandler :: ReaderT ServerData Handler String
+getGameHandler = do 
+  ref <- ask
+  liftIO (readIORef (messageRef ref))
+              
+
 gameHandler :: ServerT GameApi GameHandler
-gameHandler = pure "Hello tokens"
-              :<|> postGameHandler
+gameHandler = getGameHandler :<|> postGameHandler
  
 apiHandler :: ServerData -> Server AppApi
 apiHandler sd = blogHandler

daeec0a75963874bebbbaf1325748d2f5355d39d
Toby Smyth <tobs169@gmail.com>
2017-03-11 14:53:53 +0000

    use generated function

--- static/purescript/src/Component/Blog.purs
+++ static/purescript/src/Component/Blog.purs
@@ -1,29 +1,35 @@
 module Component.Blog where
 
 import Prelude
-
-import Data.Generic (class Generic, gEq, gCompare)
-import Data.Functor.Coproduct (Coproduct())
-
 import Halogen
-import Halogen.HTML.Indexed as H
-import Halogen.HTML.Properties.Indexed as P
-
 import Model
-import Component.Article (article)
-import Component.Article as Article
-import Control.Monad.Aff 
+import Control.Monad.Aff
 import Control.Monad.Aff.Class
 import Control.Monad.Aff.Free
-import Network.HTTP.Affjax (AJAX())
-import Network.HTTP.Affjax as Ajax
-import Control.Monad.Except(runExcept)
-
+import Control.Monad.Except.Trans
+import Control.Monad.Reader.Trans
+import Data.Argonaut.Generic.Aeson
+import Data.Generic
 import Data.Foreign
 import Data.Foreign.Class
+import Component.Article as Article
+import Halogen.HTML.Indexed as H
+import Halogen.HTML.Properties.Indexed as P
+import Network.HTTP.Affjax as Ajax
+import CSS.Transform (offset)
+import Component.Article (article)
+import Control.Monad.Except (runExcept)
+import DOM.HTML.HTMLTemplateElement (content)
 import Data.Either (Either(..))
+import Data.Functor.Coproduct (Coproduct)
+import Data.Generic (class Generic, gEq, gCompare)
 import Data.Maybe (Maybe(..))
-
+import Network.HTTP.Affjax (AJAX)
+import Servant.PureScript.Affjax (errorToString)
+import Servant.PureScript.Settings (SPSettings_(..))
+import Servant.Subscriber.Internal (doCallback)
+import ServerAPI (getApiBlogs, SPParams_(..))
+import System.BlogRepository (Blog(..))
 newtype ArticleSlot = ArticleSlot ArticleId
 
 derive instance articleSlot :: Generic ArticleSlot
@@ -32,11 +38,11 @@ instance ordArticleSlot :: Ord ArticleSlot where compare = gCompare
 
 type BlogEffects eff = (ajax :: AJAX | eff)
 
-type State = Blog
+type State = BlogState
 data Query a =
   Load a
 
-type FState g = ParentState Blog (Article.FState g) Query Article.FQuery g ArticleSlot
+type FState g = ParentState BlogState(Article.FState g) Query Article.FQuery g ArticleSlot
 type FQuery = Coproduct Query (ChildF ArticleSlot Article.FQuery)
 type BlogDSL g = ParentDSL State (Article.FState g) Query Article.FQuery g ArticleSlot
 type BlogHTML g = ParentHTML (Article.FState g) Query Article.FQuery g ArticleSlot
@@ -65,13 +71,21 @@ blog = lifecycleParentComponent {render, eval, peek: Nothing, initializer: Just 
                              , component: article}
 
 getBlogs :: forall eff. Aff (ajax :: AJAX | eff) (Array Article)
-getBlogs = 
-  let url = "api/blogs" in
-  do
-    resp <- Ajax.get url
-    pure case
-      runExcept $ readJSON resp.response :: F (Array Article)
-      of
-        Right ids -> ids
-        Left err -> [Article {title: "ERROR", contents: show err, visible: true, id: -1}]
+getBlogs = do
+  ebs <- runExceptT $ runReaderT getApiBlogs settings
+  case ebs of 
+    Left err -> pure [Article {title: "ERROR", contents: errorToString err, visible: true, id: -1}]
+    Right bs -> pure (convert <$> bs)
+
+convert :: Blog -> Article
+convert (Blog {id, title, content}) = initialArticle id title content
+
+settings :: SPSettings_ SPParams_
+settings =  SPSettings_ { 
+                    encodeJson : encodeJson
+                  , decodeJson : decodeJson
+                  , toURLPiece : gShow
+                  , params : SPParams_ {
+                      baseURL : "http://localhost:8080/"
+                  }}
 

45911da0a915b761e605c51a40c10d6c91e0a3b8
Toby Smyth <tobs169@gmail.com>
2017-03-12 01:16:16 +0000

    babel

--- webpack.config.js
+++ webpack.config.js
@@ -3,33 +3,35 @@ const webpack = require('webpack');
 
 const ignorePaths = /node_modules|bower_components/
 const config = 
-      {  devtool: 'source-map'
-        , devServer: { contentBase: '.'
-                       , port: 4008
-                       , stats: 'errors-only'
-                     }
-        , entry: './static/entry'
-        , output: {path: path.join(__dirname, "static/dist")
-                   , pathinfo: true
-                   , filename: "bundle.js"
-                  }
-        , module: { rules: [ { test: /\.purs$/
-                           , loader: 'purs-loader'
-                           , options: { src: [ 'bower_components/purescript-*/src/**/*.purs', 'static/purescript/src/**/*.purs', 'static/purescript/generated/**/*.purs' ]
-                                    , bundle: false
-                                    , psc: 'node_modules/purescript/bin/psc.js'
-                                    , pscBundle: 'node_modules/purescript/bin/psc-bundle.js'
-                                    , pscArgs: { sourceMaps: true }
-                                    , pscIde: true
-                                    }
-                               }
-                            
-                         , { test: /\.js$/
-                           , use: 'source-map-loader'
-                           , exclude: ignorePaths
-                           }
-                         ]
-                  }
+      { devtool: 'source-map'
+      , devServer: { contentBase: '.'
+                     , port: 4008
+                     , stats: 'errors-only'
+                   }
+      , entry: './static/entry'
+      , output: {path: path.join(__dirname, "static/dist")
+                 , pathinfo: true
+                 , filename: "bundle.js"
+                }
+      , module: { 
+          rules: [ { test: /\.purs$/
+                   , loader: 'purs-loader'
+                   , options: { src: [ 'bower_components/purescript-*/src/**/*.purs', 'static/purescript/src/**/*.purs', 'static/purescript/generated/**/*.purs' ]
+                            , bundle: false
+                            , psc: 'node_modules/purescript/bin/psc.js'
+                            , pscBundle: 'node_modules/purescript/bin/psc-bundle.js'
+                            , pscArgs: { sourceMaps: true }
+                            , pscIde: true
+                            , watch: true
+                            }
+                       }
+                       , { test: /\.js$/
+                         , loader: 'babel-loader'
+                         , exclude: ignorePaths
+                         , options: {presets: ['es2015']}
+                         }
+                       ]
+                }
 };
 
 module.exports = config;

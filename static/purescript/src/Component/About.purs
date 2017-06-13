130cf3891db461ab584452e2205829880411fe03
Toby Smyth <tobs169@gmail.com>
2017-06-13 15:00:33 +0100

    about page

--- static/purescript/src/Component/About.purs
+++ static/purescript/src/Component/About.purs
@@ -3,25 +3,33 @@ module Component.About where
 import Prelude
 import Halogen
 
-import Halogen.HTML.Core (className)
 import Halogen.HTML as H
 import Halogen.HTML.Events as E
 import Halogen.HTML.Properties as P
-import Halogen.HTML.Properties (pixels)
 
+import Data.Void
+import Data.Maybe (Maybe(..))
 foreign import mePic :: String
 
 type State = Unit
 
 data Query a = Noop a
 
-about :: forall g . Component State Query g 
-about = component {render, eval}
+type Input = Unit
+
+type Message = Void
+
+about :: forall g . Component H.HTML Query Input Message g 
+about = component 
+  { initialState: const unit
+  , render
+  , eval
+  , receiver: const Nothing}
             where
               render :: State -> ComponentHTML Query
               render _ = H.div_
                          [ H.h2_ [H.text "Me"]
-                         , H.img [P.src mePic, P.height $ pixels 200]
+                         , H.img [P.src mePic, P.height 200]
                          ,  H.h2_ [H.text "Source"]
                          , H.div_
                            [ H.a
@@ -32,5 +40,5 @@ about = component {render, eval}
                            ]
                          ]
 
-              eval :: Query ~> (ComponentDSL State Query g)
+              eval :: Query ~> (ComponentDSL State Query Message g)
               eval (Noop a) = pure a

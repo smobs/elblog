dd0290bfe8ce02dfcbe684fb1c0960930cbf62dc
Toby Smyth <tobs169@gmail.com>
2017-06-16 16:32:04 +0100

    Dom events

--- static/purescript/src/Component/LoginWidget.purs
+++ static/purescript/src/Component/LoginWidget.purs
@@ -7,22 +7,19 @@ import Data.Maybe (Maybe(..))
 import Halogen.HTML.Events as E
 import Halogen.HTML as H
 import Halogen.HTML.Properties as P
+import DOM.Event.KeyboardEvent as K
 
 type State = {auth :: Maybe AuthToken, login :: String}
 
 
 onlyForKey :: forall props g ev.                       
-  (Functor g) => Number                      
-                   -> ({ "keyCode" :: Number  
-                       | props                  
-                       }                      
+  (Functor g) => String                      
+                   -> (K.KeyboardEvent                      
                        -> g (Maybe ev)     
                       )                       
-                      -> { "keyCode" :: Number
-                         | props                
-                         }
+                      -> K.KeyboardEvent
                          -> g (Maybe ev)
-onlyForKey i input  = (\ (a@{keyCode}) -> (if keyCode == 13.0 then id else const Nothing) <$> input a)
+onlyForKey i input  = (\ (ke) -> (if K.code ke == i then id else const Nothing) <$> input ke)
 
-render update setAuth = H.div_ $ [ H.text "Username",  H.input [E.onValueInput (E.input update), E.onKeyPress  (onlyForKey 13.0 (E.input_ setAuth))], H.button [E.onClick (E.input_ setAuth)] [H.text "Login"] ]
+render update setAuth = H.div_ $ [ H.text "Username",  H.input [E.onValueInput (E.input update), E.onKeyPress  (onlyForKey "Enter" (E.input_ setAuth))], H.button [E.onClick (E.input_ setAuth)] [H.text "Login"] ]
               

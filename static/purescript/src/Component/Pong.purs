06c9bc698094e217974acb76964071b07285937a
Toby Smyth <tobs169@gmail.com>
2017-06-18 21:08:15 +0100

    Pong

--- static/purescript/src/Component/Pong.purs
+++ static/purescript/src/Component/Pong.purs
@@ -1,9 +1,10 @@
 module Component.Pong where 
 
+
 import Halogen.HTML.Events as E
 import Halogen.HTML as H
 import Halogen.HTML.Properties as P
-
+import Control.Monad.Aff
 import Control.Monad.Aff.Free (class Affable)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Class (class MonadEff, liftEff)
@@ -16,69 +17,68 @@ import Data.Int (ceil)
 import Data.Maybe (Maybe(..))  
 import Data.Semigroup ((<>))
 import Graphics.Canvas (CANVAS)
-import Halogen (ComponentDSL, ComponentHTML, Component, HalogenEffects, action, lifecycleComponent, liftH, get, modify, subscribe, eventSource_, EventSource)
-import Halogen.HTML.Properties (pixels)
+import Halogen (ComponentDSL, ComponentHTML, Component, SubscribeStatus(Listening), action, lifecycleComponent, get, modify, subscribe, eventSource_, EventSource)
 import Pong (sendCommand, PongState(..), DirY(..), Move(..), Player(..), PongCommand(..), renderPong, initialPongState)
-import Prelude (class Monad, type (~>), Unit, bind, pure, show, void, ($), (<<<))
-
+import Prelude
+import Control.Monad.Aff.AVar(AVAR)
+import DOM.Event.KeyboardEvent as K
 
 type State = { pong :: PongState, loop :: Maybe IntervalId}
 
 initial :: State
 initial = {pong: initialPongState, loop: Nothing}
 type KeyCode = Number
-data Query a = NewGame a | StopGame a | Input KeyCode a | StepGame a
+data Query a = NewGame a | StopGame a | Input K.KeyboardEvent a | StepGame a
 
-type PongEffects eff = (canvas :: CANVAS , timer :: TIMER, console :: CONSOLE | eff)
+type PongEffects eff = (canvas :: CANVAS , timer :: TIMER, console :: CONSOLE, avar :: AVAR | eff)
 
-game :: forall g eff. (Monad g, Affable (HalogenEffects(PongEffects eff)) g, MonadEff (HalogenEffects(PongEffects eff)) g) => Component State Query g 
-game = lifecycleComponent {render, eval, initializer: Just (action NewGame), finalizer: Just (action StopGame)}
+game :: forall eff. Component H.HTML Query Unit Void (Aff (PongEffects eff))
+game = lifecycleComponent {initialState: const initial, receiver: const Nothing, render, eval, initializer: Just (action NewGame), finalizer: Just (action StopGame)}
             where
               render :: State -> ComponentHTML Query
               render {pong} = H.div [] [ H.canvas [ P.id_ canvasName
-                                             , E.onKeyDown  (E.input (\ {keyCode} -> Input keyCode))
+                                             , E.onKeyDown  (E.input (\ ke-> Input ke))
                                              , P.tabIndex 0
-                                             , P.height $ pixels $ ceil canvasSize.h
+                                             , P.height $ ceil canvasSize.h
                                              , P.width
-                                               $ pixels
                                                $ ceil canvasSize.w ]
                                         , renderGameInfo pong]
 
-              eval :: Query ~> (ComponentDSL State Query g)
+              eval :: Query ~> (ComponentDSL State Query Void (Aff (PongEffects eff)))
               eval (NewGame a) = do
                 s <- get
-                liftH <<< liftEff $ log "initialise pong"
-                liftH <<< liftEff $ renderPong canvasName s.pong
+                liftEff $ log "initialise pong"
+                liftEff $ renderPong canvasName s.pong
                 subscribe $ stepper
                 pure a
-              eval (Input keyCode a) =
-                case lookupCommand keyCode of
+              eval (Input ke a) =
+                case lookupCommand (K.code ke) of
                   Nothing -> pure a
                   Just com -> do
                     modify (\s -> s {pong = sendCommand (com) s.pong})
                     s <- get
-                    liftH <<< liftEff $ renderPong canvasName s.pong
+                    liftEff $ renderPong canvasName s.pong
                     pure a
               eval (StopGame a) = do
                 s <- get
                 case s.loop of
                   (Just i) -> do
-                    liftH <<< liftEff $ log "clear pong"
-                    liftH <<< liftEff $ clearInterval i
+                    liftEff $ log "clear pong"
+                    liftEff $ clearInterval i
                     pure a
                   Nothing -> pure a
                 pure a
               eval (StepGame a) = do
                 modify (\s -> s { pong = sendCommand Step s.pong })
                 s <- get
-                liftH <<< liftEff $ renderPong canvasName s.pong
+                liftEff $ renderPong canvasName s.pong
                 pure a
 
-lookupCommand :: KeyCode -> Maybe PongCommand
-lookupCommand c | c == 83.0 = Just $ MovePlayer { player: One, move: Direction Down}
-                | c == 87.0 = Just $ MovePlayer { player: One, move: Direction Up}
-                | c == 40.0 = Just $ MovePlayer { player: Two, move: Direction Down}
-                | c == 38.0 = Just $ MovePlayer { player: Two, move: Direction Up}
+lookupCommand :: String -> Maybe PongCommand
+lookupCommand c | c == "a" = Just $ MovePlayer { player: One, move: Direction Down}
+                | c == "z" = Just $ MovePlayer { player: One, move: Direction Up}
+                | c == "j" = Just $ MovePlayer { player: Two, move: Direction Down}
+                | c == "m" = Just $ MovePlayer { player: Two, move: Direction Up}
                 | otherwise = Nothing
 renderGameInfo :: PongState -> ComponentHTML Query 
 renderGameInfo (PongState {score:{one, two} }) = H.div [] [ H.text (show one <> " : " <> show two) 
@@ -100,5 +100,5 @@ loop :: forall eff . Eff (timer :: TIMER | eff) Unit -> Eff (timer:: TIMER | eff
 loop a = let x = void $ setInterval 50 a in
   x
 
-stepper ::  forall g eff. (Monad g, Affable (HalogenEffects(PongEffects eff)) g) => EventSource Query g
-stepper = eventSource_ loop (pure $ action StepGame)
+stepper ::  forall eff. EventSource Query (Aff (PongEffects eff))
+stepper = eventSource_ loop (StepGame Listening)
